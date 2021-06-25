610131b1b460bf334f96aa41067dc4a162bcc92d
imraneouelfaquir98 <imrane.ouelfaquir@gmail.com>
2021-06-25 01:58:09 +0100

    Missing number problem

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -7,11 +7,10 @@ int main()
 	long long int n;
 	cin >> n;
 	int array[n-1];
-	for (int i = 0; i < n-1; ++i)
-		cin >> array[i];
+	for (int i = 0; i < n-1; ++i) cin >> array[i];
 	vector<bool> exist (n, false);
 	for (int i=0; i<n-1; ++i) exist[array[i]-1] = true;
-	for (int i=0; i<n; ++i)
+	for (int i=0; i<n; ++i) 
 		if(exist[i] == false) cout << i+1;
 	return 0;
 }
