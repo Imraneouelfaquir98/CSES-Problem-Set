4eaf2ad8e7c619050070914b5901f61bedc21a66
imraneouelfaquir98 <imrane.ouelfaquir@gmail.com>
2021-06-25 01:33:18 +0100

    test

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -7,10 +7,8 @@ int main(int argc, char const *argv[])
 	cin >> n;
 	cout<<n<<" ";
 	while(n != 1){
-		if( n%2 == 0)
-			n = n/2;
-		else
-			n = n*3 + 1;
+		if( n%2 == 0) n = n/2;
+		else n = n*3 + 1;
 		cout<<n<<" ";
 	}
 	return 0;
